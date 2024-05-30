f6c015ac5dbebc50ddee9d79f1ee786bb02e96f7
davicruz23 <davifieledeus@gmail.com>
2024-05-30 10:15:48 -0300

    ajustes

--- src/main/java/com/teste/sementes/SementesApplication.java
+++ src/main/java/com/teste/sementes/SementesApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class SementesApplication {
 
